a8a1e139238422e89dda99f59936c684fc4c4694
Enrique Carrillo <enriquecarrillo88@gmail.com>
2015-01-29 16:55:24 +0100

    Changed page is rendered in the index

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   resources :suggestions
 
-  root to: 'suggestion_box#index'
+  root to: 'suggestions#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
