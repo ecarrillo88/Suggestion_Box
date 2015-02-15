07f869083e5e7a27ba38c5235719822fb47d7f52
Enrique Carrillo <enriquecarrillo88@gmail.com>
2015-02-15 11:05:15 +0100

    Added validation email

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,6 +3,8 @@ class Comment < ActiveRecord::Base
 
   validates :author, presence: true,
                      length: { maximum: 25 }
-  validates :email, presence: :true
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true,
+                    format: { with: VALID_EMAIL_REGEX }
   validates :text, presence: :true
 end
--- app/models/suggestion.rb
+++ app/models/suggestion.rb
@@ -2,6 +2,8 @@ class Suggestion < ActiveRecord::Base
   has_many :comments, dependent: :destroy
   
   validates :title, presence: :true
-  validates :email, presence: :true
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true,
+                    format: { with: VALID_EMAIL_REGEX }
   validates :comment, presence: :true
 end
