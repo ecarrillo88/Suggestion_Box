6fc22d7785fb953eaeb31caf69f4f34720714fa0
Enrique Carrillo <enriquecarrillo88@gmail.com>
2015-02-22 19:12:21 +0100

    Added title and description to the header dynamically

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,4 +5,20 @@ module ApplicationHelper
     yield presenter if block_given?
     presenter
   end
+  
+  def set_header_title(header_title)
+    return '<h1>Suggestion Box</h1>'.html_safe if header_title.empty?
+    return header_title
+  end
+  
+  def set_header_description(header_description)
+    return 'Drop your comments, questions, requests and complaints' if header_description.empty?
+    return header_description
+  end
+  
+  def set_header_button(header_button)
+    return link_to 'Create a new Suggestion', new_suggestion_path, class: 'btn btn-primary btn-lg' if header_button.empty?
+    return "" if header_button == 'no'
+    return header_button
+  end
 end
