379b77c90472005d2978ffbbfe3e6306c4ff43f2
Enrique Carrillo <enriquecarrillo88@gmail.com>
2015-04-25 11:37:47 +0200

    Fixed bug in suggestions form

--- app/helpers/suggestions_helper.rb
+++ app/helpers/suggestions_helper.rb
@@ -1,2 +1,7 @@
 module SuggestionsHelper
+  def form_url(current_url)
+    action = current_url.split('/')
+    return create_suggestion_path if action.last == 'new'
+    return update_suggestion_path if action.last == 'edit'
+  end
 end
