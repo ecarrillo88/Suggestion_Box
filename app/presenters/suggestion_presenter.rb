7452a8d71e9b1d901f1fec503beadc82033a2fd6
Enrique <enriquecarrillo88@gmail.com>
2015-05-05 21:14:49 +0200

    Added the address of the suggestions on the homepage

--- app/presenters/suggestion_presenter.rb
+++ app/presenters/suggestion_presenter.rb
@@ -1,48 +1,48 @@
 class SuggestionPresenter < BasePresenter
   presents :suggestion
-  
+
   def comments
     suggestion.comments.where(visible: true).size
   end
-  
+
   def supporters
     suggestion.comments.where(visible: true, support: true).size
   end
-  
+
   def has_map?
     !suggestion.latitude.nil? && !suggestion.longitude.nil?
   end
-  
+
   def has_comments?
     comments > 0
   end
-  
+
   def has_image1?
     !suggestion.image1_id.nil?
   end
-  
+
   def has_image2?
     !suggestion.image2_id.nil?
   end
-  
+
   def has_images?
     has_image1? || has_image2?
   end
-  
+
   def activated?
     suggestion.visible?
   end
-  
+
   def progress_in_favour
     total = suggestion.comments.where("vote = 1 OR vote = 3").size
     in_favour = suggestion.comments.where(vote: 1).size
     if total != 0
-      return ((in_favour.to_f / total) * 100).to_i 
+      return ((in_favour.to_f / total) * 100).to_i
     else
       return 0
     end
   end
-  
+
   def progress_against
     against = suggestion.comments.where(vote: 3).size
     if against != 0
@@ -50,5 +50,21 @@ class SuggestionPresenter < BasePresenter
     else
       return 0
     end
+  end
+
+  def has_address?
+    !suggestion.reverse_geocode.nil?
+  end
+
+  def street
+    suggestion.reverse_geocode.split(',')[0]
+  end
+
+  def postal_code
+    suggestion.reverse_geocode.split(',')[2].to_i
+  end
+
+  def district
+    suggestion.reverse_geocode.split(',')[2][7..-1]
   end
 end
