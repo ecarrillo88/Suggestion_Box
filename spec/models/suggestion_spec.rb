4230d7f856e5cde1270ddc8e0dd3cb685bcab506
Enrique <enriquecarrillo88@gmail.com>
2015-05-25 20:24:57 +0200

    Updated suggestion model specs

--- spec/models/suggestion_spec.rb
+++ spec/models/suggestion_spec.rb
@@ -3,43 +3,34 @@ require 'rails_helper'
 RSpec.describe Suggestion, type: :model do
 
   before(:each) do
-    @suggestion1 = Suggestion.create({category: Suggestion.category[:suggestion], title: 'Suggestion of the year', author: 'Anonymous', email: 'my_email@email.com', comment: 'My suggestion!', latitude: 40.4167754, longitude: -3.7037901999999576})
-    @suggestion2 = Suggestion.create({category: Suggestion.category[:complaint], title: 'Complaint', author: 'Anonymous', email: 'my_email@email.com', comment: 'My complaint!', latitude: 40.3963198127118, longitude: -3.716254234313965})
+    @suggestion1 = Suggestion.create({category: Suggestion.category[:suggestion], title: 'Suggestion of the year', author: 'Anonymous', email: 'my_email@email.com', comment: 'My suggestion!', latitude: nil, longitude: nil, visible: true})
+    @suggestion2 = Suggestion.create({category: Suggestion.category[:complaint], title: 'Complaint', author: 'Anonymous', email: 'my_email@email.com', comment: 'My complaint!', latitude: nil, longitude: nil, visible: true})
+    @suggestion3 = Suggestion.create({category: Suggestion.category[:suggestion], title: 'Suggestion', author: 'Anonymous', email: 'my_email@email.com', comment: 'Another suggestion', latitude: nil, longitude: nil, visible: true})
   end
 
   context "search_filter" do
-    it "should filter by category" do
-      category = Suggestion.category[:suggestion]
+    it "should show all suggestions" do
+      category = nil
       title = nil
       address = nil
       distance = nil
-      suggestion = Suggestion.search_filter(category, title, address, distance)
-      expect(suggestion.first).to eq(@suggestion1)
+      suggestions = Suggestion.search_filter(category, title, address, distance)
+      expect(suggestions).to eq([@suggestion3, @suggestion2, @suggestion1])
+    end
+
+    it "should filter by category" do
+      suggestion = Suggestion.search_filter(Suggestion.category[:complaint], nil, nil, nil)
+      expect(suggestion).to eq([@suggestion2])
     end
 
     it "should filter by title" do
-      suggestion = Suggestion.search_filter(nil, 'Suggestion', nil, nil)
-      expect(suggestion.first).to eq(@suggestion1)
+      suggestion = Suggestion.search_filter(nil, 'Suggestion of the year', nil, nil)
+      expect(suggestion).to eq([@suggestion1])
     end
 
     it "should filter by title with multiple words" do
-      suggestion = Suggestion.search_filter(nil, 'Suggestion the', nil, nil)
-      expect(suggestion.first).to eq(@suggestion1)
-    end
-
-    it "should filter by address" do
-      suggestion = Suggestion.search_filter(nil, nil, 'Puerta del Sol Madrid', 0.5)
-      expect(suggestion.first).to eq(@suggestion1)
-    end
-
-    it "should filter by distance" do
-      suggestions = Suggestion.search_filter(nil, nil, 'Puerta del Sol Madrid', 10)
-      expect(suggestions).to eq([@suggestion1, @suggestion2])
-    end
-
-    it "should filter by category, title and address" do
-      suggestion = Suggestion.search_filter(Suggestion.category[:complaint], 'Complaint', 'Calle Fuenlabrada Madrid', 0.5)
-      expect(suggestion.first).to eq(@suggestion2)
+      suggestion = Suggestion.search_filter(nil, 'Suggestion', nil, nil)
+      expect(suggestion).to eq([@suggestion3, @suggestion1])
     end
   end
 end
