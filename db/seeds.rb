b84a558be3a35c020efd633eaabab420abc1e659
Miguel Angel Fernandez <miguelangel@peertransfer.com>
2015-08-11 16:21:32 +0000

    add suggestion to the seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,3 +10,6 @@ CityCouncilDomain.create(domain: 'city_council.gov') # development
 CityCouncilDomain.create(domain: 'mailinator.com')   # prodution
 CityCouncilResponsiblePerson.create(name: 'Juan', last_name: 'Pardo Gutierrez', email: 'juanpardo@email.com')     # development
 CityCouncilResponsiblePerson.create(name: 'Maria', last_name: 'Pérez García', email: 'mariaperez@mailinator.com') # prodution
+
+suggestion = Suggestion.create({category: Suggestion.category[:suggestion], title: 'Suggestion of the year', author: 'Anonymous', email: 'my_email@email.com', comment: 'My suggestion!', latitude: nil, longitude: nil, visible: true, closed: 0, image1_id: 1, image2_id: 1})
+Comment.create(suggestion: suggestion, visible:true, email: 'my_other_email@email.com', vote: Comment::IN_FAVOUR, support: true, author: 'A real person!', text: 'Hey this is a comment')
